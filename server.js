61eebd7cdbd1055dbfd13a62e43fe3d69cc73b47
ubrmnz <zxvf@tutanota.com>
2022-08-04 08:42:35 +0530

    feat: web socket key

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-import process from "node:process"
-import http from "node:http"
+import http from "node:http";
+import process from "node:process";
 
 const PORT = 5000;
 
-const app = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('<h1>Learning WebSockets</h1>')
-})
+  res.end("<h1>Learning WebSockets</h1>");
+});
 
-app.listen(PORT, () => {
-  console.log(`Server listening at port => ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`Server listening at port => ${PORT}`);
+});
+
+server.on("upgrade", (req, socket, head) => {
+  const { "sec-websocket-key": SOCKET_KEY } = req.headers;
+
+  console.log(SOCKET_KEY);
+  //console.log({ head: req.headers });
+});
+
+// error handling
+
+[("uncaughtException", "unhandledRejection")].forEach((e) => {
+  process.on(e, (err) => {
+    console.error(`Something went wrong: ${e}, msg: ${err.stack || err}`);
+  });
+});
